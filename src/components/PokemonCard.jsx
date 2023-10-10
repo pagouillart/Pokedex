bc86bbc83054269dc78783f84b3ff5199833a3d8
Pierre-Adrien Gouillart <pagouillart@hotmail.fr>
2023-10-10 14:52:21 +0200

    adding PokemonCard

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,23 @@
 function PokemonCard() {
-return (<figure>
-<img src ="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" ></img>
-<figcaption>Bulbasaur</figcaption>
-</figure>
-);
+    const pokemon = pokemonList[1];
+
+    return (<figure>
+        {pokemon.imgSrc ? <img src={pokemon.imgSrc}/> : <p>"???"</p>}
+        <figcaption>{pokemon.name}</figcaption>
+
+    </figure>
+    );
+
 }
-  export default PokemonCard;
+export default PokemonCard;
+
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];

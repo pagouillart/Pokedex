6108f3a775d3bcc5029097b32a581a79ca4fd0b7
Pierre-Adrien Gouillart <pagouillart@hotmail.fr>
2023-10-11 14:24:05 +0200

    adding proptypes

--- src/App.jsx
+++ src/App.jsx
@@ -19,3 +19,5 @@ const pokemonList = [
   },
 ];
 export default App;
+
+
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,13 +1,25 @@
-function PokemonCard({pokemon}) {
-    
-    
+import PropTypes from "prop-types";
 
-    return (<figure>
-        {pokemon.imgSrc ? <img src={pokemon.imgSrc}/> : <p>"???"</p>}
+function PokemonCard({ pokemon }) {
+
+    
+        
+
+    return (
+    <figure>
+        {pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : <p>"???"</p>}
         <figcaption>{pokemon.name}</figcaption>
 
     </figure>
     );
+    }
+
+    PokemonCard.propTypes = {
+        pokemon : PropTypes.shape({
+          name: PropTypes.string.isRequired,
+          imgSrc : PropTypes.string
+        }).isRequired,
+
 }
 export default PokemonCard;
 
